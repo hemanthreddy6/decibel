8e9ddf480728b47b62314c2b5f1092b810216d04
Tuhil <Tuhil>
2024-11-06 15:53:43 +0530

    made the debug print statements be indented

--- Semantic/semantic_analyser.cpp
+++ Semantic/semantic_analyser.cpp
@@ -109,8 +109,6 @@ DataType *current_return_type;
 // is for const declarations.
 int handle_declaration(Stype *node, bool has_dtype, bool is_const) {
     // if it's already in the current scope level, then it's a redeclaration.
-    cout << "hello: " << current_scope_table << " " << current_scope << endl;
-    cout << "node-text: " << node->children[0]->text << endl;
     if (symbol_table[current_scope_table][current_scope].count(
             node->children[0]->text)) {
         // this is just for error handling and printing the error with the
@@ -259,21 +257,21 @@ bool is_basic_type(DataType *type, int token) {
 void traverse_ast(Stype *node) {
     switch (node->node_type) {
     case NODE_ROOT:
-        cout << "Root node" << endl;
+        cout << string(current_scope, '\t') << "Root node" << endl;
         traverse_ast(node->children[0]);
         traverse_ast(node->children[2]);
         // main block processed last
         traverse_ast(node->children[1]);
         break;
     case NODE_STATEMENTS:
-        cout << "Statements node" << endl;
+        cout << string(current_scope, '\t') << "Statements node" << endl;
         for (Stype *child : node->children) {
             traverse_ast(child);
         }
         break;
 
     case NODE_MAIN_BLOCK:
-        cout << "Main block node" << endl;
+        cout << string(current_scope, '\t') << "Main block node" << endl;
         // before traversing the statements node, do stuff to make the scope be
         // main block and stuff
         symbol_table[current_scope_table].push_back(
@@ -283,7 +281,7 @@ void traverse_ast(Stype *node) {
         current_scope = 0;
         break;
     case NODE_READ_STATEMENT:
-        cout << "Read statement node" << endl;
+        cout << string(current_scope, '\t') << "Read statement node" << endl;
         traverse_ast(node->children[0]);
         if (node->data_type->is_primitive && !node->data_type->is_vector) {
             if (node->data_type->basic_data_type == AUDIO) {
@@ -295,7 +293,7 @@ void traverse_ast(Stype *node) {
         }
         break;
     case NODE_PRINT_STATEMENT:
-        cout << "Print statement node" << endl;
+        cout << string(current_scope, '\t') << "Print statement node" << endl;
         traverse_ast(node->children[0]);
         if (node->children[0]->data_type->is_primitive) {
             if (node->children[0]->data_type->basic_data_type == AUDIO) {
@@ -308,7 +306,7 @@ void traverse_ast(Stype *node) {
 
         break;
     case NODE_DECLARATION_STATEMENT:
-        cout << "Declaration statement node" << endl;
+        cout << string(current_scope, '\t') << "Declaration statement node" << endl;
         // traverse the expression first
         traverse_ast(node->children[1]);
 
@@ -316,7 +314,7 @@ void traverse_ast(Stype *node) {
             break;
         break;
     case NODE_DECLARATION_STATEMENT_WITH_TYPE:
-        cout << "Declaration statement node with type" << endl;
+        cout << string(current_scope, '\t') << "Declaration statement node with type" << endl;
         // traverse the expression first
         traverse_ast(node->children[1]);
 
@@ -324,7 +322,7 @@ void traverse_ast(Stype *node) {
             break;
         break;
     case NODE_CONST_DECLARATION_STATEMENT:
-        cout << "Const declaration statement node" << endl;
+        cout << string(current_scope, '\t') << "Const declaration statement node" << endl;
         // traverse the expression first
         traverse_ast(node->children[1]);
 
@@ -332,27 +330,27 @@ void traverse_ast(Stype *node) {
             break;
         break;
     case NODE_DATA_TYPE:
-        cout << "Data type statement node" << endl;
+        cout << string(current_scope, '\t') << "Data type statement node" << endl;
         break;
     case NODE_INT_LITERAL:
-        cout << "Int literal node" << endl;
+        cout << string(current_scope, '\t') << "Int literal node" << endl;
         node->data_type = new DataType(INT);
         break;
     case NODE_FLOAT_LITERAL:
-        cout << "Float literal node" << endl;
+        cout << string(current_scope, '\t') << "Float literal node" << endl;
         node->data_type = new DataType(FLOAT);
         break;
     case NODE_STRING_LITERAL:
-        cout << "String literal node" << endl;
+        cout << string(current_scope, '\t') << "String literal node" << endl;
         node->data_type = new DataType(STRING);
         break;
     case NODE_IDENTIFIER:
-        cout << "Identifier node" << endl;
+        cout << string(current_scope, '\t') << "Identifier node" << endl;
         if (handle_identifier_reference(node))
             break;
         break;
     case NODE_PARAMETER_LIST:
-        cout << "NODE_PARAMETER_LIST" << endl;
+        cout << string(current_scope, '\t') << "NODE_PARAMETER_LIST" << endl;
         node->data_type = new DataType();
         node->data_type->is_primitive = false;
         for (Stype *parameter : node->children) {
@@ -361,7 +359,7 @@ void traverse_ast(Stype *node) {
         }
         break;
     case NODE_PARAMETER:
-        cout << "NODE_PARAMETER" << endl;
+        cout << string(current_scope, '\t') << "NODE_PARAMETER" << endl;
         traverse_ast(node->children[1]);
         if (handle_parameter_declaration(node->children[0],
                                          node->children[1])) {
@@ -370,13 +368,13 @@ void traverse_ast(Stype *node) {
         node->data_type = node->children[1]->data_type;
         break;
     case NODE_RETURNABLE_STATEMENTS:
-        cout << "NODE_RETURNABLE_STATEMENTS" << endl;
+        cout << string(current_scope, '\t') << "NODE_RETURNABLE_STATEMENTS" << endl;
         for (Stype *child : node->children) {
             traverse_ast(child);
         }
         break;
     case NODE_RETURN_STATEMENT:
-        cout << "NODE_RETURN_STATEMENT" << endl;
+        cout << string(current_scope, '\t') << "NODE_RETURN_STATEMENT" << endl;
         if (!node->children.size()) {
             if (current_return_type != NULL) {
                 // error
@@ -407,22 +405,22 @@ void traverse_ast(Stype *node) {
         }
         break;
     case NODE_FUNCTION_ARGUMENTS:
-        cout << "NODE_FUNCTION_ARGUMENTS" << endl;
+        cout << string(current_scope, '\t') << "NODE_FUNCTION_ARGUMENTS" << endl;
         for (Stype *child : node->children) {
             traverse_ast(child);
         }
         break;
     case NODE_ARGUMENT_LIST:
-        cout << "NODE_ARGUMENT_LIST" << endl;
+        cout << string(current_scope, '\t') << "NODE_ARGUMENT_LIST" << endl;
         for (Stype *child : node->children) {
             traverse_ast(child);
         }
         break;
     case NODE_OMITTED_PARAMETER:
-        cout << "NODE_OMITTED_PARAMETER" << endl;
+        cout << string(current_scope, '\t') << "NODE_OMITTED_PARAMETER" << endl;
         break;
     case NODE_FUNCTION_CALL: {
-        cout << "NODE_FUNCTION_CALL" << endl;
+        cout << string(current_scope, '\t') << "NODE_FUNCTION_CALL" << endl;
         traverse_ast(node->children[0]);
         traverse_ast(node->children[1]);
         if (node->data_type->is_primitive) {
@@ -459,7 +457,7 @@ void traverse_ast(Stype *node) {
         break;
     }
     case NODE_NORMAL_ASSIGNMENT_STATEMENT:
-        cout << "NODE_NORMAL_ASSIGNMENT_STATEMENT" << endl;
+        cout << string(current_scope, '\t') << "NODE_NORMAL_ASSIGNMENT_STATEMENT" << endl;
         // traverse the expression first
         traverse_ast(node->children[1]);
         // then, traverse the assignable value to check if it's a valid
@@ -468,38 +466,38 @@ void traverse_ast(Stype *node) {
         // TODO: check if data types match/can convert using the function
         break;
     case NODE_PLUS_EQUALS_ASSIGNMENT_STATEMENT:
-        cout << "NODE_PLUS_EQUALS_ASSIGNMENT_STATEMENT" << endl;
+        cout << string(current_scope, '\t') << "NODE_PLUS_EQUALS_ASSIGNMENT_STATEMENT" << endl;
         break;
     case NODE_MINUS_EQUALS_ASSIGNMENT_STATEMENT:
-        cout << "NODE_MINUS_EQUALS_ASSIGNMENT_STATEMENT" << endl;
+        cout << string(current_scope, '\t') << "NODE_MINUS_EQUALS_ASSIGNMENT_STATEMENT" << endl;
         break;
     case NODE_MULT_EQUALS_ASSIGNMENT_STATEMENT:
-        cout << "NODE_MULT_EQUALS_ASSIGNMENT_STATEMENT" << endl;
+        cout << string(current_scope, '\t') << "NODE_MULT_EQUALS_ASSIGNMENT_STATEMENT" << endl;
         break;
     case NODE_DIVIDE_EQUALS_ASSIGNMENT_STATEMENT:
-        cout << "NODE_DIVIDE_EQUALS_ASSIGNMENT_STATEMENT" << endl;
+        cout << string(current_scope, '\t') << "NODE_DIVIDE_EQUALS_ASSIGNMENT_STATEMENT" << endl;
         break;
     case NODE_MOD_EQUALS_ASSIGNMENT_STATEMENT:
-        cout << "NODE_MOD_EQUALS_ASSIGNMENT_STATEMENT" << endl;
+        cout << string(current_scope, '\t') << "NODE_MOD_EQUALS_ASSIGNMENT_STATEMENT" << endl;
         break;
     case NODE_OR_EQUALS_ASSIGNMENT_STATEMENT:
-        cout << "NODE_OR_EQUALS_ASSIGNMENT_STATEMENT" << endl;
+        cout << string(current_scope, '\t') << "NODE_OR_EQUALS_ASSIGNMENT_STATEMENT" << endl;
         break;
     case NODE_POWER_EQUALS_ASSIGNMENT_STATEMENT:
-        cout << "NODE_POWER_EQUALS_ASSIGNMENT_STATEMENT" << endl;
+        cout << string(current_scope, '\t') << "NODE_POWER_EQUALS_ASSIGNMENT_STATEMENT" << endl;
         break;
     case NODE_DISTORTION_EQUALS_ASSIGNMENT_STATEMENT:
-        cout << "NODE_DISTORTION_EQUALS_ASSIGNMENT_STATEMENT" << endl;
+        cout << string(current_scope, '\t') << "NODE_DISTORTION_EQUALS_ASSIGNMENT_STATEMENT" << endl;
         break;
     case NODE_CONTINUE_STATEMENT:
-        cout << "NODE_CONTINUE_STATEMENT" << endl;
+        cout << string(current_scope, '\t') << "NODE_CONTINUE_STATEMENT" << endl;
         break;
     case NODE_BREAK_STATEMENT:
-        cout << "NODE_BREAK_STATEMENT" << endl;
+        cout << string(current_scope, '\t') << "NODE_BREAK_STATEMENT" << endl;
         break;
     case NODE_LOOP_REPEAT_STATEMENT:
     {
-        cout << "NODE_LOOP_REPEAT_STATEMENT" << endl;
+        cout << string(current_scope, '\t') << "NODE_LOOP_REPEAT_STATEMENT" << endl;
         current_scope++;
         symbol_table[current_scope_table].push_back(
             unordered_map<string, StEntry>());
@@ -518,7 +516,7 @@ void traverse_ast(Stype *node) {
     }
     case NODE_LOOP_UNTIL_STATEMENT:
     {
-        cout << "NODE_LOOP_UNTIL_STATEMENT" << endl;
+        cout << string(current_scope, '\t') << "NODE_LOOP_UNTIL_STATEMENT" << endl;
         current_scope++;
         symbol_table[current_scope_table].push_back(
             unordered_map<string, StEntry>());
@@ -537,7 +535,7 @@ void traverse_ast(Stype *node) {
     }
     case NODE_LOOP_GENERAL_STATEMENT:
     {
-        cout << "NODE_LOOP_GENERAL_STATEMENT" << endl;
+        cout << string(current_scope, '\t') << "NODE_LOOP_GENERAL_STATEMENT" << endl;
         current_scope++;
         symbol_table[current_scope_table].push_back(unordered_map<string, StEntry>());
         bool is_int = false;
@@ -568,7 +566,7 @@ void traverse_ast(Stype *node) {
         break;
     }
     case NODE_IF_STATEMENT:
-        cout << "NODE_IF_STATEMENT" << endl;
+        cout << string(current_scope, '\t') << "NODE_IF_STATEMENT" << endl;
         current_scope++;
         symbol_table[current_scope_table].push_back(
             unordered_map<string, StEntry>());
@@ -580,13 +578,13 @@ void traverse_ast(Stype *node) {
         traverse_ast(node->children[3]);
         break;
     case NODE_OR_STATEMENTS:
-        cout << "NODE_OR_STATEMENTS" << endl;
+        cout << string(current_scope, '\t') << "NODE_OR_STATEMENTS" << endl;
         for (Stype *child : node->children) {
             traverse_ast(child);
         }
         break;
     case NODE_OR_STATEMENT:
-        cout << "NODE_OR_STATEMENT" << endl;
+        cout << string(current_scope, '\t') << "NODE_OR_STATEMENT" << endl;
         current_scope++;
         symbol_table[current_scope_table].push_back(
             unordered_map<string, StEntry>());
@@ -596,7 +594,7 @@ void traverse_ast(Stype *node) {
         current_scope--;
         break;
     case NODE_OTHERWISE_STATEMENT:
-        cout << "NODE_OTHERWISE_STATEMENT" << endl;
+        cout << string(current_scope, '\t') << "NODE_OTHERWISE_STATEMENT" << endl;
         if (node->children.size() == 0) {
             // do nothing
         } else {
@@ -609,24 +607,24 @@ void traverse_ast(Stype *node) {
         }
         break;
     case NODE_LOAD_STATEMENT:
-        cout << "NODE_LOAD_STATEMENT" << endl;
+        cout << string(current_scope, '\t') << "NODE_LOAD_STATEMENT" << endl;
         break;
     case NODE_PLAY_STATEMENT:
-        cout << "NODE_PLAY_STATEMENT" << endl;
+        cout << string(current_scope, '\t') << "NODE_PLAY_STATEMENT" << endl;
         break;
     case NODE_SAVE_STATEMENT:
-        cout << "NODE_SAVE_STATEMENT" << endl;
+        cout << string(current_scope, '\t') << "NODE_SAVE_STATEMENT" << endl;
         break;
     case NODE_NORMAL_FUNCTION:
-        cout << "NODE_NORMAL_FUNCTION" << endl;
+        cout << string(current_scope, '\t') << "NODE_NORMAL_FUNCTION" << endl;
         handle_function_expression(node);
         break;
     case NODE_INLINE_FUNCTION:
-        cout << "NODE_INLINE_FUNCTION" << endl;
+        cout << string(current_scope, '\t') << "NODE_INLINE_FUNCTION" << endl;
         handle_function_expression(node);
         break;
     case NODE_UNARY_INVERSE_EXPR:
-        cout << "NODE_UNARY_INVERSE_EXPR" << endl;
+        cout << string(current_scope, '\t') << "NODE_UNARY_INVERSE_EXPR" << endl;
         traverse_ast(node->children[0]);
         node->data_type = node->children[0]->data_type;
         if (!is_basic_type(node->data_type, AUDIO)) {
@@ -637,80 +635,80 @@ void traverse_ast(Stype *node) {
         }
         break;
     case NODE_UNARY_LOGICAL_NOT_EXPR:
-        cout << "NODE_UNARY_LOGICAL_NOT_EXPR" << endl;
+        cout << string(current_scope, '\t') << "NODE_UNARY_LOGICAL_NOT_EXPR" << endl;
 
         break;
     case NODE_UNARY_PLUS_EXPR:
-        cout << "NODE_UNARY_PLUS_EXPR" << endl;
+        cout << string(current_scope, '\t') << "NODE_UNARY_PLUS_EXPR" << endl;
         break;
     case NODE_UNARY_MINUS_EXPR:
-        cout << "NODE_UNARY_MINUS_EXPR" << endl;
+        cout << string(current_scope, '\t') << "NODE_UNARY_MINUS_EXPR" << endl;
         break;
     case NODE_BOOL_LITERAL:
-        cout << "NODE_BOOL_LITERAL" << endl;
+        cout << string(current_scope, '\t') << "NODE_BOOL_LITERAL" << endl;
         break;
     case NODE_INDEX:
-        cout << "NODE_INDEX" << endl;
+        cout << string(current_scope, '\t') << "NODE_INDEX" << endl;
         break;
     case NODE_SLICE:
-        cout << "NODE_SLICE" << endl;
+        cout << string(current_scope, '\t') << "NODE_SLICE" << endl;
         break;
     case NODE_POWER_EXPR:
-        cout << "NODE_POWER_EXPR" << endl;
+        cout << string(current_scope, '\t') << "NODE_POWER_EXPR" << endl;
         break;
     case NODE_DISTORTION_EXPR:
-        cout << "NODE_DISTORTION_EXPR" << endl;
+        cout << string(current_scope, '\t') << "NODE_DISTORTION_EXPR" << endl;
         break;
     case NODE_MULT_EXPR:
-        cout << "NODE_MULT_EXPR" << endl;
+        cout << string(current_scope, '\t') << "NODE_MULT_EXPR" << endl;
         break;
     case NODE_DIVIDE_EXPR:
-        cout << "NODE_DIVIDE_EXPR" << endl;
+        cout << string(current_scope, '\t') << "NODE_DIVIDE_EXPR" << endl;
         break;
     case NODE_MOD_EXPR:
-        cout << "NODE_MOD_EXPR" << endl;
+        cout << string(current_scope, '\t') << "NODE_MOD_EXPR" << endl;
         break;
     case NODE_SPEEDUP_EXPR:
-        cout << "NODE_SPEEDUP_EXPR" << endl;
+        cout << string(current_scope, '\t') << "NODE_SPEEDUP_EXPR" << endl;
         break;
     case NODE_SPEEDDOWN_EXPR:
-        cout << "NODE_SPEEDDOWN_EXPR" << endl;
+        cout << string(current_scope, '\t') << "NODE_SPEEDDOWN_EXPR" << endl;
         break;
     case NODE_PLUS_EXPR:
-        cout << "NODE_PLUS_EXPR" << endl;
+        cout << string(current_scope, '\t') << "NODE_PLUS_EXPR" << endl;
         break;
     case NODE_MINUS_EXPR:
-        cout << "NODE_MINUS_EXPR" << endl;
+        cout << string(current_scope, '\t') << "NODE_MINUS_EXPR" << endl;
         break;
     case NODE_SUPERPOSITION_EXPR:
-        cout << "NODE_SUPERPOSITION_EXPR" << endl;
+        cout << string(current_scope, '\t') << "NODE_SUPERPOSITION_EXPR" << endl;
         break;
     case NODE_LE_EXPR:
-        cout << "nœud le expression" << endl;
+        cout << string(current_scope, '\t') << "nœud le expression" << endl;
         break;
     case NODE_LEQ_EXPR:
-        cout << "NODE_LEQ_EXPR" << endl;
+        cout << string(current_scope, '\t') << "NODE_LEQ_EXPR" << endl;
         break;
     case NODE_GE_EXPR:
-        cout << "NODE_GE_EXPR" << endl;
+        cout << string(current_scope, '\t') << "NODE_GE_EXPR" << endl;
         break;
     case NODE_GEQ_EXPR:
-        cout << "NODE_GEQ_EXPR" << endl;
+        cout << string(current_scope, '\t') << "NODE_GEQ_EXPR" << endl;
         break;
     case NODE_EQUALS_EXPR:
-        cout << "NODE_EQUALS_EXPR" << endl;
+        cout << string(current_scope, '\t') << "NODE_EQUALS_EXPR" << endl;
         break;
     case NODE_NOT_EQUALS_EXPR:
-        cout << "NODE_NOT_EQUALS_EXPR" << endl;
+        cout << string(current_scope, '\t') << "NODE_NOT_EQUALS_EXPR" << endl;
         break;
     case NODE_LOGICAL_AND_EXPR:
-        cout << "NODE_LOGICAL_AND_EXPR" << endl;
+        cout << string(current_scope, '\t') << "NODE_LOGICAL_AND_EXPR" << endl;
         break;
     case NODE_LOGICAL_OR_EXPR:
-        cout << "NODE_LOGICAL_OR_EXPR" << endl;
+        cout << string(current_scope, '\t') << "NODE_LOGICAL_OR_EXPR" << endl;
         break;
     case NODE_ASSIGNABLE_VALUE:
-        cout << "NODE_ASSIGNABLE_VALUE" << endl;
+        cout << string(current_scope, '\t') << "NODE_ASSIGNABLE_VALUE" << endl;
         traverse_ast(node->children[0]);
         node->data_type = node->children[0]->data_type;
         // TODO: handle vector index and slice
@@ -721,7 +719,7 @@ void traverse_ast(Stype *node) {
              << endl;
         break;
     default:
-        cout << "Big bad error: bruh you forgot to handle this node" << endl;
+        cout << string(current_scope, '\t') << "Big bad error: bruh you forgot to handle this node" << endl;
     }
 }
 
