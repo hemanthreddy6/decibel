aed89f48518d753473b359ec7764344f234f0778
Tuhil <Tuhil>
2024-12-07 00:27:41 +0530

    WTF IT WORKEDgit add .git add .

--- CodeGen/decibel_stdlib.cpp
+++ CodeGen/decibel_stdlib.cpp
@@ -41,6 +41,7 @@ void writeValue(vector<char>&file, unsigned int value, unsigned int count) {
 }
 
 // Audio functions
+extern "C"{
 struct Audio load_audio(char* filename) {
     ifstream file(filename, ios::binary);
     vector<char> fileBytes((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
@@ -175,4 +176,4 @@ void play_audio(struct Audio audio_var) {
 
     remove(temp_file);
 }
-
+}
